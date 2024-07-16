3ad5a19d20fabda20ba381edf8d8190958d04d5f
pdy <1412656741@qq.com>
2024-07-17 05:46:05 +0800

    添加basePath

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     output:"export",
-    publicRuntimeConfig: {
-        staticFolder: '/static/db.json', // 这里可以是相对路径或JSON内容
-    }
+    basePath: '/resource',
 };
